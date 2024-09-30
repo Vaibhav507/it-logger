125bb1b0e098abdbab97c0f0c52b93b045d73385
unknown <vaibhavpratham507@gmail.com>
2024-09-30 17:12:27 +0530

    IT-logger UI completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js"
 import './App.css';
+import Logs from "./components/logs/Logs";
+import SearchBar from "./components/layout/SearchBar";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // Init Materialize Js
+    M.AutoInit();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <AddTechModal />
+        <EditLogModal />
+        <TechListModal />
+        <Logs />
+      </div>
     </div>
   );
 }
